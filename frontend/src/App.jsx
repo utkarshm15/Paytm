62b649545c062d21dc7ac4f3109db7f67cc5573a
Utkarsh Mishra <utkarshmishra2410@gmail.com>
2024-06-13 20:05:07 +0530

    added frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,29 @@
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { Heading } from "./components/Heading";
+import { SubHeading } from "./components/SubHeading";
+import { InputBox } from "./components/InputBox";
+import { Button } from "./components/Button";
+import { Warning } from "./components/Warning";
+import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
 function App() {
 
   return (
     <div>
-        Hello world
+      <BrowserRouter>
+          <Routes>
+              <Route path="/signup" element={<SignUp/>}/>
+              <Route path="/signin" element={<SignIn/>}/>
+              <Route path="/dashboard" element={<Dashboard/>}/>
+              <Route path="/send" element={<SendMoney/>}/>
+          </Routes>
+      </BrowserRouter>
     </div>
+    
   )
+
 }
 
 export default App
